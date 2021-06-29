2d94453833c7b819b4b7b00c91fa156b09765a94
Nahom Abebe <58399654+nahom-hub@users.noreply.github.com>
2021-06-29 14:02:12 -0500

    Project one update done

--- project0/src/main/java/com/revature/models/Favorite.java
+++ project0/src/main/java/com/revature/models/Favorite.java
@@ -3,44 +3,54 @@ package com.revature.models;
 public class Favorite {
 	private int favorite_id;
 	private int my_day;
+	private String myFavoritDay;
 	private String my_color;
 	private String my_month;
-	private String  myDay;
-	
-	
 	public Favorite() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
-
-	public Favorite(int my_day, String my_color, String my_month, String string) {
-		super();
+	public int getFavorite_id() {
+		return favorite_id;
+	}
+	public void setFavorite_id(int favorite_id) {
+		this.favorite_id = favorite_id;
+	}
+	public int getMy_day() {
+		return my_day;
+	}
+	public void setMy_day(int my_day) {
 		this.my_day = my_day;
+	}
+	public String getMyFavoritDay() {
+		return myFavoritDay;
+	}
+	public void setMyFavoritDay(String myFavoritDay) {
+		this.myFavoritDay = myFavoritDay;
+	}
+	public String getMy_color() {
+		return my_color;
+	}
+	public void setMy_color(String my_color) {
 		this.my_color = my_color;
+	}
+	public String getMy_month() {
+		return my_month;
+	}
+	public void setMy_month(String my_month) {
 		this.my_month = my_month;
 	}
-
-
-	@Override
-	public String toString() {
-		return "Favorite [favorite_id=" + favorite_id + ", my_day=" + my_day + ", my_color=" + my_color + ", my_month="
-				+ my_month + "]";
-	}
-
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + favorite_id;
+		result = prime * result + ((myFavoritDay == null) ? 0 : myFavoritDay.hashCode());
 		result = prime * result + ((my_color == null) ? 0 : my_color.hashCode());
 		result = prime * result + my_day;
 		result = prime * result + ((my_month == null) ? 0 : my_month.hashCode());
 		return result;
 	}
-
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -51,6 +61,11 @@ public class Favorite {
 			return false;
 		Favorite other = (Favorite) obj;
 		if (favorite_id != other.favorite_id)
+			return false;
+		if (myFavoritDay == null) {
+			if (other.myFavoritDay != null)
+				return false;
+		} else if (!myFavoritDay.equals(other.myFavoritDay))
 			return false;
 		if (my_color == null) {
 			if (other.my_color != null)
@@ -66,56 +81,22 @@ public class Favorite {
 			return false;
 		return true;
 	}
-
-
-	public int getFavorite_id() {
-		return favorite_id;
+	@Override
+	public String toString() {
+		return "Favorite [favorite_id=" + favorite_id + ", my_day=" + my_day + ", myFavoritDay=" + myFavoritDay
+				+ ", my_color=" + my_color + ", my_month=" + my_month + "]";
 	}
-
-
-	public void setFavorite_id(int favorite_id) {
-		this.favorite_id = favorite_id;
-	}
-
-
-	public int getMy_day() {
-		return my_day;
-	}
-
-
-	public void setMy_day(int my_day) {
+	public Favorite(int my_day, String myFavoritDay, String my_color, String my_month) {
+		super();
 		this.my_day = my_day;
-	}
-
-
-	public String getMy_color() {
-		return my_color;
-	}
-
-
-	public void setMy_color(String my_color) {
+		this.myFavoritDay = myFavoritDay;
 		this.my_color = my_color;
-	}
-
-
-	public String getMy_month() {
-		return my_month;
-	}
-
-
-	public void setMy_month(String my_month) {
 		this.my_month = my_month;
 	}
-
-
-	public String getMyDay() {
-		return myDay;
-	}
-
-
-	public void setMyDay(String myDay) {
-		this.myDay = myDay;
-	}
+	
+	
+	
+	
 	
 	
 	
--- project0/src/main/java/com/revature/models/Menu.java
+++ project0/src/main/java/com/revature/models/Menu.java
@@ -6,172 +6,188 @@ import java.util.Scanner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
 import com.revature.daos.FavoriteDao;
 
 public class Menu {
- //all of the menu
-	//Player ply = new Player();
+	// all of the menu
+	// Player ply = new Player();
 	FavoriteDao astr = new FavoriteDao();
-	//Player ply = new Player();
-	
-	public void display(){
-		
-		
-		boolean displayMenu = true; //this toggles whether the menu continues after user input
-		Scanner scan = new Scanner(System.in); //Scanner object to parse user input
+	// Player ply = new Player();
+
+	public void display() {
+
+		boolean displayMenu = true; // this toggles whether the menu continues after user input
+		Scanner scan = new Scanner(System.in); // Scanner object to parse user input
 		final Logger log = LogManager.getLogger(Menu.class);
-		
-		//greeting
+
+		// greeting
 		System.out.println("*====================================================*");
 		System.out.println("Welcome to the guessing games System");
 		System.out.println("*====================================================*");
-		while(displayMenu) {
+		while (displayMenu) {
 			System.out.println("----------------");
 			System.out.println("CHOOSE AN OPTION BELLOW TO ACCESS THE DATA");
 			System.out.println("*--------------------------------------------------*");
-			
-			//menu options
+
+			// menu options
 			System.out.println("Players -> show all player");
 			System.out.println("Add -> add a new player ");
 			System.out.println("Delete player -> Terminate a player");
 			System.out.println("Favorite -> show Favorite");
 			System.out.println("exit the program -> exit the application");
-			
-			
+
 			String input = scan.nextLine();
-			
-			
-switch(input) {
-			
+
+			switch (input) {
+
 			case "Players": {
-				
-				//this is the  logger
+
+				// this is the logger
 				log.info("user selected all players");
-				
+
 				System.out.println("Gathering all employees...");
 
-				//List of player that gets populated by the getplayers method in our AstrolgyDao
-				List<Player> players = astr.getPlayers(); 
-				
-				//Print out each Employee from the List one by one for the user to see
-				for(Player e : players) {
-					System.out.println(e);
+				// List of player that gets populated by the getplayers method in our
+				// AstrolgyDao
+				List<Player> players = astr.getPlayers();
+
+				// Print out each Employee from the List one by one for the user to see
+				for (Player e : players) {
+					System.out.println(
+							e.getPlayer_id() + ")" + e.getF_name() + ")" + e.getL_name() + ")" + e.getBirth_date());
+
+					break;
 				}
-				
-				break;
 			}
-			
 			case "Add": {
-				
-				//we need to prompt the user for the employee's name, and their role id
-				//we'll have to come up with some functionality to get the current date for the hire_date field
+
+				// we need to prompt the user for the employee's name, and their role id
+				// we'll have to come up with some functionality to get the current date for the
+				// hire_date field
 				System.out.println("Enter playerId: ");
 				int player_Id = scan.nextInt();
 				scan.nextLine();
 				System.out.println("Enter player First Name:");
 				String f_name = scan.nextLine();
-				
+
 				System.out.println("Enter player Last Name:");
 				String l_name = scan.nextLine();
-			
-				//String birth_date = scan.nextLine();
-				
-				 //because without any nextLine, your enter keystroke will be grabbed as the next input
-				
-				//Given all this information, we'll create a new player object to send to a DAO method
-				//This is using the all-args minus employee_id constructor
-				Player newPlayer = new Player(player_Id,f_name, l_name, "xxxx-xx-xx"); //placeholder for hire_date, will be changed
-				
-				//Put the new player into the addPlayer() method in the AstrologyDao
+
+				// String birth_date = scan.nextLine();
+
+				// because without any nextLine, your enter keystroke will be grabbed as the
+				// next input
+
+				// Given all this information, we'll create a new player object to send to a DAO
+				// method
+				// This is using the all-args minus employee_id constructor
+				Player newPlayer = new Player(player_Id, f_name, l_name, "xxxx-xx-xx"); // placeholder for hire_date,
+																						// will be changed
+
+				// Put the new player into the addPlayer() method in the AstrologyDao
 				astr.addPlayer(newPlayer);
-				
+
 				break;
 			}
 
 			case "Delete player": {
-				
+
 				System.out.println("These are the players on the roster... who will you terminate?");
-				
-				//this is using the already existing getEmployees() method
-				List<Player> players = astr.getPlayers(); 
-				
-				//Print out each Employee from the List one by one for the user to see
-				for(Player e : players ) {
+
+				// this is using the already existing getEmployees() method
+				List<Player> players = astr.getPlayers();
+
+				// Print out each Employee from the List one by one for the user to see
+				for (Player e : players) {
 					System.out.println(e);
 				}
-				
+
 				System.out.println("------------------------------");
-				
+
 				System.out.println("Enter the player id of the player to terminate:");
 				log.warn("Hey are you sure you want to delete a player");
 				int idInput = scan.nextInt();
 				scan.nextLine();
-				
+
 				astr.removeplayer(idInput);
-				
+
 				break;
 			}
-			
-			case "Favorite":{
+
+			case "Favorite": {
 				System.out.println("Please enter your name");
 				String fName = scan.nextLine();
-				
+
 				log.info("Player entered the name");
-				
+
 				System.out.println("Hello" + " " + fName);
 				System.out.println("Welcome to play guessing game");
-				
+
 				List<Favorite> favorites = astr.getFavorites();
 				System.out.println("What day do you guess i love more?");
 				String playerGuessDay = scan.nextLine();
-				System.out.println("Good trying");
-				
-				List<Player> players = astr.getPlayers(); 
+				//System.out.println("Good trying");
+				for (Favorite e : favorites) {
+					String x =  e.getMyFavoritDay();
+					if(playerGuessDay.equalsIgnoreCase(x)) {
+						System.out.println("You are right ...applaud");
+					}else {
+						System.out.println("No but it was good trying");
+					}
+				}
+				List<Player> players = astr.getPlayers();
 				System.out.println("What color do you guess i love more?");
 				String myFav_color = scan.nextLine();
-				System.out.println("Good trying");
-				
-				
-				
+				//System.out.println("Good trying");
+				for (Favorite e : favorites) {
+					String y = e.getMy_color();
+					if(myFav_color.equalsIgnoreCase(y)) {
+						System.out.println("You are right ...applaud");
+					}else {
+						System.out.println("No but it was good trying");
+					}
+				}
 				
 				System.out.println("===================================");
 				System.out.println("What month do you guess i love more?");
 				String myFav_month = scan.nextLine();
-				System.out.println("Good trying");
-				
-				//this is the  logger
+				//System.out.println("Good trying");
+				for (Favorite e : favorites) {
+					String z =   e.getMy_month();
+					if( myFav_month.equalsIgnoreCase(z)) {
+						System.out.println("You are right ...applaud");
+					}else {
+						System.out.println("No but it was good trying");
+					}
+				}
+				// this is the logger
 				log.warn("Hello player check the answers bellow if your were attempt");
-				
+
 				System.out.println("===================================");
 				System.out.println("Greate here are my favorites day, color and months anyway.");
-				for(Favorite e : favorites) {
-					System.out.println(e);
-					
+				for (Favorite e : favorites) {
+					System.out.print("My favorite day is " + e.getMyFavoritDay() + "\n " + "My Favorite color is "
+							+ e.getMy_color() + "\n " + "My favorite month is " + e.getMy_month() + "\n ");
+
 				}
 				System.out.println("Thank you for trying" + " " + fName);
 				break;
-				
+
 			}
-			
-			
-			
-			
-			
+
 			case "exit": {
 				System.out.println("byeeee");
 				displayMenu = false;
 				break;
 			}
-			
+
 			default: {
-				System.out.println("Didn't catch that... try again"); //in case user input doesn't match any cases
+				System.out.println("Didn't catch that... try again"); // in case user input doesn't match any cases
 				break;
 			}
-			
-			
+
 			}
-			
+
 		}
 	}
 }
